4038599a57efb7defd708a2a5266464876024b4c
Jaz <62318169+jasrayatwal@users.noreply.github.com>
2025-07-14 17:26:03 -0700

    Add basic expense model code

---
+++ app/models/expense.py
@@ -0,0 +1,20 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Expense(db.Model):
+  __tablename__ = 'expenses'
+
+  if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(30), nullable=False)
+  expense_owner = db.Column(db.Integer, nullable=False)
+  status = db.Column(db.String(20), nullable=False)
+
+  def to_dict(self):
+      return {
+          'id': self.id,
+          'description': self.description,
+          'expense_owner': self.expense_owner,
+          'status': self.status
+      }
