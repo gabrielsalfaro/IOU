d9762130a3e9147a4fe6359e99ff718d3ffc1edd
Jaz <62318169+jasrayatwal@users.noreply.github.com>
2025-07-14 17:40:36 -0700

    Add basic info for expense members table to model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .db import db
 from .user import User
 from .db import environment, SCHEMA
 from .expense import Expense
+from .expense_member import ExpenseMember
---
+++ app/models/expense_member.py
@@ -0,0 +1,22 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class ExpenseMember(db.Model):
+    __tablename__ = 'expense_members'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    expense_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('expenses.id')), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    amount_owed = db.Column(db.Numeric(precision=10, scale=2))
+    settled = db.Column(db.Boolean, nullable=False)
+
+    def to_dict(self):
+        return {
+          'id': self.id,
+          'expense_id': self.expense_id,
+          'user_id': self.user_id,
+          'amount_owed': self.amount_owed,
+          'settled': self.settled
+        }
