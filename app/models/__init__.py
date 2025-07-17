27bfbf0a93b77e66b535120453b51db42bf131ab
Andrewmc123 <andrewmcclain123@gmail.com>
2025-07-16 21:02:07 -0400

    init py fix

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,6 @@
 from .db import db
-from .user import User
 from .db import environment, SCHEMA
-<<<<<<< Updated upstream
+from .user import User
 from .expense import Expense
 from .expense_member import ExpenseMember
-=======
 from .payments import Payment
->>>>>>> Stashed changes
