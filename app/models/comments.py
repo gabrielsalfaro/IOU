cfbb0d83b244e86809a514efb9f3130eadf87533
gabesauce <alfaro.gabe@gmail.com>
2025-07-17 19:02:26 -0700

    comments model, migration update

--- app/models/comments.py
+++ app/models/comments.py
@@ -1,27 +1,27 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from flask_login import UserMixin
 from sqlalchemy import CheckConstraint
-import datetime
+from datetime import datetime
 
-class Friends(db.Model):
-    __table__ = 'comments'
+class Comment(db.Model):
+    __tablename__ = 'comments'
 
     if environment == "production": 
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    expense_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
-    content = db.Column(db.String())
+    expense_id = db.Column(db.Integer, db.ForeignKey('expenses.id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    content = db.Column(db.String(), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now) # check date stuff
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
-def to_dict(self):
-    return {
-        'id': self.id,
-        'expense_id': self.expense_id,
-        'user_id': self.user_id,
-        'content': self.content,
-        'created_at': self.created_at,
-        'updated_at': self.updated_at
-    }
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'expense_id': self.expense_id,
+            'user_id': self.user_id,
+            'content': self.content,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
