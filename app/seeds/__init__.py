c4c4743691752a3a88925861c842fc902356e5f6
Jaz <62318169+jasrayatwal@users.noreply.github.com>
2025-07-24 18:53:12 -0700

    try fixing render issues, fixing ordering of seeding b/c of dependencies

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -21,28 +21,28 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_expenses()
-        undo_expense_members()
         undo_comments()
-        undo_friends()
         undo_payments()
+        undo_expense_members()
+        undo_friends()
+        undo_expenses()
+        undo_users()
     seed_users()
     seed_expenses()
-    seed_expense_members()
-    seed_comments()
     seed_friends()
+    seed_expense_members()
     seed_payments()
+    seed_comments()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_expenses()
-    undo_expense_members()
     undo_comments()
-    undo_friends()
     undo_payments()
+    undo_expense_members()
+    undo_friends()
+    undo_expenses()
+    undo_users()
     # Add other undo functions here
