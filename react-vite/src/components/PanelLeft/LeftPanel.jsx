e872f2ea697f0b8fe15387a1f89e0b8e8987b3f7
gabesauce <alfaro.gabe@gmail.com>
2025-07-25 15:08:33 -0700

    add mini friends list to left panel

--- react-vite/src/components/PanelLeft/LeftPanel.jsx
+++ react-vite/src/components/PanelLeft/LeftPanel.jsx
@@ -1,19 +1,88 @@
-// import React from 'react'
+import { useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchFriends } from '../../redux/friends';
 import './LeftPanel.css'
 import { NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const LeftPanel = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const friendsObj = useSelector(state => state.friends?.friends || {});
+  
+  // Memoize and sort
+  const friends = useMemo(() => Object.values(friendsObj).sort((a, b) => {
+    const nameA = a.friend.firstname.toLowerCase();
+    const nameB = b.friend.firstname.toLowerCase();
+    return nameA.localeCompare(nameB);
+  }), [friendsObj]);
+
+  useEffect(() => {
+    dispatch(fetchFriends());
+    // dispatch(fetchPendingFriends());
+  }, [dispatch]);
+
+  const isFriendsPage = location.pathname === "/friends";
+
   return (
     <>
-      <div className="left-panel">
-        <NavLink to='/dashboard' className='panel-link'>Dashboard</NavLink>
-        <NavLink to='/expenses' className='panel-link'>All Expenses</NavLink>
-        <NavLink to='/payments' className='panel-link'>Payment History</NavLink>
-        <NavLink to='/friends' className='panel-link'>Friends</NavLink> {/*  temp, for my sanitiy */}
+      <div className="left-panel-contianer">
+        <div className="left-panel">
+          <NavLink to='/dashboard' className='panel-link'>Dashboard</NavLink>
+          <NavLink to='/expenses' className='panel-link'>All Expenses</NavLink>
+          <NavLink to='/payments' className='panel-link'>Payment History</NavLink>
+          <NavLink to='/friends' className='panel-link'>Friends</NavLink> {/*  temp, for my sanitiy */}
+        </div>
+        <div className="left-panel-friends">
+          {/* display short friends list here when not in Friends page */}
+          {isFriendsPage ? ('') : (
+          <div className="mini-friends-list">
+            {friends.length === 0 ? (
+              <p className="friend-item">No friends yet.</p>
+            ) : (
+              <ul>
+                <div className="friends-group-name" title='Group Feature Coming Soon!'>
+                  <center>
+                    <button className="friend-group-button" onClick={() => alert('Group Feature Coming Soon!')}>
+                      the squad
+                    </button>
+                  </center>
+                </div>
+                {friends.slice(0,3).map(friend => (
+                  <li key={friend.id} className="friend-item">
+
+                    <div className="friend-content">
+                      <div className="friend-info">
+                        <NavLink to={`/users/${friend.friend.id}`} >
+                          {friend.friend.firstname} {friend.friend.lastname}
+                        </NavLink>
+                      </div>
+                        
+
+                      {/* <div className="friend-actions"> */}
+                        {/* <button onClick={() => handleDelete(friend.friend.id)} className='friend-remove-button'>Remove</button> */}
+                        {/* <OpenModalButton
+                          buttonText="Remove"
+                          className="friend-remove-button"
+                          modalComponent={<FriendsAddRemoveModal actionType="remove" friend={friend.friend} />}
+                        /> */}
+
+                      {/* </div> */}
+                    </div>
+
+                  </li>
+                ))}
+                <NavLink to="/friends" className="view-all-friends-link">
+                          + View All Friends
+                        </NavLink>
+              </ul>
+            )}
+          </div>
+          )}
+        </div>
       </div>
-      <div className="left-panel-friends">
-        {/* display short friends list here when not in Friends page */}
-      </div>
+
 
     </>
   )
