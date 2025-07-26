9f9e7a7f749595e82168c605bb2ea1d1f88acc5b
Jaz <62318169+jasrayatwal@users.noreply.github.com>
2025-07-25 18:40:01 -0700

    render test

--- app/models/friend.py
+++ app/models/friend.py
@@ -10,12 +10,12 @@ class Friend(db.Model):
         __table_args__ = (
             CheckConstraint("status IN ('friends', 'pending')", name="check_status_valid"),
             {'schema': SCHEMA}
-            
+
         )
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    friend_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False) #changed for prod
+    friend_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False) #changed for prod
     status = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
